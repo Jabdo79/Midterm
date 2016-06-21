f09551cd555d3ef4ac37fa38d1d75a83cda440e6
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2016-06-21 13:37:43 -0400

    tiny sc commit

--- src/PosTerminal.java
+++ src/PosTerminal.java
@@ -99,7 +99,7 @@ public class PosTerminal {
 			displayCategory("Snacks");
 			break;
 		case 6:
-			Payment.receipt(cart);//
+			Payment.receipt(cart, sc);//
 			orderNumber +=1;
 			break;
 		case 7:
