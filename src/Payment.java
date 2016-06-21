55afa40f10704dcc0e24433a7a47c4c6cc19aeed
candeortiz <candelaria@10.251.2.88>
2016-06-21 16:28:24 +0200

    commit

--- src/Payment.java
+++ src/Payment.java
@@ -2,17 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Payment {
-	
-	private static double subtotal;
-	
-	public static void calcSubtotal(ArrayList <Product> userProducts){
 
-		for (int i = 0; i<userProducts.size();i++)
-			subtotal += userProducts.get(i).getProductPrice() * userProducts.get(i).getProductQuantity();
+	private static double subtotal;
+
+	public static void calcSubtotal(ArrayList<Product> userProducts) {
 
 	}
-	
-	public static void cash (Scanner sc) {
+
+	public static void cash(Scanner sc) {
 		System.out.print("Cash: ");
 		double tender = sc.nextDouble();
 		double change = tender - subtotal;
@@ -20,10 +17,22 @@ public class Payment {
 	}
 
 	public static int check;
-	
+
 	public static int credit;
-	
-	public static void receipt(ArrayList userProducts) {
+
+	public static void receipt(ArrayList<Product> userProducts) {
+
+		for (int i = 0; i < userProducts.size(); i++) {
+
+			System.out.print(userProducts.get(i).getProductName()
+					+ "\t"
+					+ (userProducts.get(i).getProductQuantity())
+					+ "\t\t$"
+					+ ((userProducts.get(i).getProductPrice()) * (userProducts
+							.get(i).getProductQuantity())));
+
+			// call: CalcSubtotal();
+
+		}
 	}
-	
 }
