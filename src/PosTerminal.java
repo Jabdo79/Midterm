4ab8cf6fee26e571644a7ac3e2d5fc301efa41bb
Jonathan Abdo <jabdo79@gmail.com>
2016-06-20 23:59:30 -0400

    Add displayCategory()

--- src/PosTerminal.java
+++ src/PosTerminal.java
@@ -14,19 +14,27 @@ import java.util.Scanner;
 
 public class PosTerminal {
 	
-	public static ArrayList<Product> products;
-
+	static ArrayList<Product> products = new ArrayList<Product>();
+	static ArrayList<Product> cart = new ArrayList<Product>();
+	static int orderNumber = 0;
+	static boolean cont = true;
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
 		//Read the products from text file into arraylist of products
 		populateProducts();
 		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
 		System.out.println("Welcome to our Grocery Store!\n");
+		while (cont) {
+			//display menu
+			displayMenu();
+		}
+		
+		
+		
+		
+		
 		
 
 	}
@@ -37,7 +45,7 @@ public class PosTerminal {
 		final Charset charset = Charset.forName("US-ASCII");
 		
 		// creates a path including filename
-		Path p = Paths.get("products.txt");
+		Path p = Paths.get("src/products.txt");
 		// creates a new file using the path
 		file = new File(p.toString());
 		
@@ -67,4 +75,58 @@ public class PosTerminal {
 		if (empty)
 			System.out.println("List is empty!");
 	}
+	
+	public static void displayMenu(){
+		
+		orderNumber +=1;
+		System.out.println("Order #: " + orderNumber);
+		int choice = InputCheck.getInt(sc, "\nCategories\n1.Fruit\n2.Vegetable\n3.Meat\n4.Dairy\n5.Snacks\n6.Exit\nChoose one: (1-6) ", 1, 5);
+		
+		switch(choice){
+		case 1:
+			displayCategory("Fruit");
+			break;
+		case 2:
+			displayCategory("Vegetable");
+			break;
+		case 3:
+			displayCategory("Meat");
+			break;
+		case 4:
+			displayCategory("Dairy");
+			break;
+		case 5:
+			displayCategory("Snacks");
+			break;
+		case 6:
+			cont = false;
+			break;
+		}
+	}
+	
+	public static void displayCategory(String category){
+		ArrayList<Product> categoryList = new ArrayList<Product>();
+		boolean isEmpty = true;
+		int catMenuNum = 1;
+		
+		System.out.println("\n"+category);
+		for (int i = 0; i < products.size(); i++) {
+			if(products.get(i).getProductCategory().equalsIgnoreCase(category)){
+				categoryList.add(products.get(i));
+				
+				System.out.println(catMenuNum + "." + products.get(i).getProductName() + "\t\t$" + products.get(i).getProductPrice());
+				System.out.println("  " + products.get(i).getProductDescription());
+				
+				catMenuNum += 1;
+				isEmpty = false;
+			}
+		}
+		if(isEmpty)
+			System.out.println("Category is empty!");
+		
+		int prodChoice = InputCheck.getInt(sc, "\nWhat would you like? (1-" + catMenuNum + ") ", 1, catMenuNum);
+		int prodQty = InputCheck.getInt(sc, "How many " + categoryList.get(prodChoice-1).getProductName() + "'s would you like? ");
+		cart.add(categoryList.get(prodChoice-1).clone(prodQty));//create new method in product class that will clone name and price from object passed to it, also take qty in param.
+	}
+	
 }
