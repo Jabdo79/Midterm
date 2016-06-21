ebcd62021334667ba0be38731592a31c278ff51c
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2016-06-21 10:46:57 -0400

    Commit

--- src/Payment.java
+++ src/Payment.java
@@ -4,18 +4,24 @@ import java.util.Scanner;
 public class Payment {
 	
 	private static double subtotal;
-	
+	private static double total;
+	private static double taxes;
 	public static void calcSubtotal(ArrayList <Product> userProducts){
-
 		for (int i = 0; i<userProducts.size();i++)
 			subtotal += userProducts.get(i).getProductPrice() * userProducts.get(i).getProductQuantity();
+		taxes = subtotal * 0.06;
+		total = subtotal + taxes;
+		System.out.println("Your subtotal is: " + subtotal);
+		System.out.println("Your taxes are: " + taxes);
+		System.out.println("Your total is: " + total);
+		
 
 	}
 	
 	public static void cash (Scanner sc) {
 		System.out.print("Cash: ");
 		double tender = sc.nextDouble();
-		double change = tender - subtotal;
+		double change = tender - total;
 		System.out.println("Thank you! Your change is " + change);
 	}
 
@@ -24,6 +30,7 @@ public class Payment {
 	public static int credit;
 	
 	public static void receipt(ArrayList userProducts) {
+		
 	}
 	
 }
